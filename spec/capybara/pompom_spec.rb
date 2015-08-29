1d2872d790e4969342df3a75a1df17e570623750
Baylor Rae <baylor@thecodedeli.com>
2015-08-29 11:16:14 -0500

    remove example spec

--- spec/capybara/pompom_spec.rb
+++ spec/capybara/pompom_spec.rb
@@ -4,8 +4,4 @@ describe Capybara::PomPom do
   it 'has a version number' do
     expect(Capybara::PomPom::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
